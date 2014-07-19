c294d7db539e1c25c286aa44e864d7d59cbb9fdf
Javier Ching <jhnching@uwaterloo.ca>
2014-07-18 22:52:12 -0400

    changed db

--- PbElite/migrations/0001_initial.py
+++ PbElite/migrations/0001_initial.py
@@ -10,7 +10,8 @@ class Migration(SchemaMigration):
     def forwards(self, orm):
         # Adding model 'Login'
         db.create_table(u'PbElite_login', (
-            ('username', self.gf('django.db.models.fields.CharField')(max_length=128, primary_key=True)),
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('username', self.gf('django.db.models.fields.CharField')(unique=True, max_length=128)),
             ('password', self.gf('django.db.models.fields.CharField')(max_length=512)),
             ('salt', self.gf('django.db.models.fields.CharField')(max_length=512)),
         ))
@@ -28,21 +29,22 @@ class Migration(SchemaMigration):
 
         # Adding model 'RaspberryPi'
         db.create_table(u'PbElite_raspberrypi', (
-            ('serial_num', self.gf('django.db.models.fields.CharField')(max_length=64, primary_key=True)),
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('serial_num', self.gf('django.db.models.fields.CharField')(unique=True, max_length=64)),
             ('username', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['PbElite.User'])),
             ('model', self.gf('django.db.models.fields.CharField')(max_length=128)),
-            ('operating_system', self.gf('django.db.models.fields.CharField')(max_length=128)),
             ('street_num', self.gf('django.db.models.fields.IntegerField')()),
             ('street_name', self.gf('django.db.models.fields.CharField')(max_length=64)),
             ('city', self.gf('django.db.models.fields.CharField')(max_length=64)),
-            ('province', self.gf('django.db.models.fields.CharField')(max_length=64)),
+            ('country', self.gf('django.db.models.fields.CharField')(max_length=64)),
             ('postal_code', self.gf('django.db.models.fields.CharField')(max_length=10)),
         ))
         db.send_create_signal(u'PbElite', ['RaspberryPi'])
 
         # Adding model 'Circuit'
         db.create_table(u'PbElite_circuit', (
-            ('circuit_num', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('circuit_num', self.gf('django.db.models.fields.IntegerField')()),
             ('serial_num', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['PbElite.RaspberryPi'])),
             ('circuit_name', self.gf('django.db.models.fields.CharField')(max_length=64)),
             ('state', self.gf('django.db.models.fields.BinaryField')()),
@@ -51,11 +53,11 @@ class Migration(SchemaMigration):
 
         # Adding model 'Reading'
         db.create_table(u'PbElite_reading', (
-            ('reading_id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('circuit_num', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['PbElite.Circuit'])),
             ('voltage', self.gf('django.db.models.fields.FloatField')()),
             ('current', self.gf('django.db.models.fields.FloatField')()),
-            ('datetime', self.gf('django.db.models.fields.DateTimeField')()),
+            ('datetime', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now)),
         ))
         db.send_create_signal(u'PbElite', ['Reading'])
 
@@ -81,24 +83,26 @@ class Migration(SchemaMigration):
         u'PbElite.circuit': {
             'Meta': {'object_name': 'Circuit'},
             'circuit_name': ('django.db.models.fields.CharField', [], {'max_length': '64'}),
-            'circuit_num': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'circuit_num': ('django.db.models.fields.IntegerField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'serial_num': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['PbElite.RaspberryPi']"}),
             'state': ('django.db.models.fields.BinaryField', [], {})
         },
         u'PbElite.login': {
             'Meta': {'object_name': 'Login'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'password': ('django.db.models.fields.CharField', [], {'max_length': '512'}),
             'salt': ('django.db.models.fields.CharField', [], {'max_length': '512'}),
-            'username': ('django.db.models.fields.CharField', [], {'max_length': '128', 'primary_key': 'True'})
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '128'})
         },
         u'PbElite.raspberrypi': {
             'Meta': {'object_name': 'RaspberryPi'},
             'city': ('django.db.models.fields.CharField', [], {'max_length': '64'}),
+            'country': ('django.db.models.fields.CharField', [], {'max_length': '64'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'model': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
-            'operating_system': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
             'postal_code': ('django.db.models.fields.CharField', [], {'max_length': '10'}),
-            'province': ('django.db.models.fields.CharField', [], {'max_length': '64'}),
-            'serial_num': ('django.db.models.fields.CharField', [], {'max_length': '64', 'primary_key': 'True'}),
+            'serial_num': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '64'}),
             'street_name': ('django.db.models.fields.CharField', [], {'max_length': '64'}),
             'street_num': ('django.db.models.fields.IntegerField', [], {}),
             'username': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['PbElite.User']"})
@@ -107,8 +111,8 @@ class Migration(SchemaMigration):
             'Meta': {'object_name': 'Reading'},
             'circuit_num': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['PbElite.Circuit']"}),
             'current': ('django.db.models.fields.FloatField', [], {}),
-            'datetime': ('django.db.models.fields.DateTimeField', [], {}),
-            'reading_id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'datetime': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'voltage': ('django.db.models.fields.FloatField', [], {})
         },
         u'PbElite.user': {
--- PbElite/models.py
+++ PbElite/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+import datetime
 
 # Create your models here.
 class Login(models.Model):
-    username = models.CharField(primary_key=True, max_length=128)
+    username = models.CharField(unique=True, max_length=128)
     password = models.CharField(max_length=512)
     salt = models.CharField(max_length=512)
 
@@ -13,28 +14,26 @@ class User(models.Model):
     email = models.CharField(max_length=128)
 
 class RaspberryPi(models.Model):
-    serial_num = models.CharField(primary_key=True, max_length=64)
+    serial_num = models.CharField(unique=True, max_length=64)
     username = models.ForeignKey(User)
     model = models.CharField(max_length=128)
-    operating_system = models.CharField(max_length=128)
     street_num = models.IntegerField()
     street_name = models.CharField(max_length=64)
     city = models.CharField(max_length=64)
-    province = models.CharField(max_length=64)
+    country = models.CharField(max_length=64)
     postal_code = models.CharField(max_length=10)
 
 class Circuit(models.Model):
-    circuit_num = models.AutoField(primary_key=True)
+    circuit_num = models.IntegerField()
     serial_num = models.ForeignKey(RaspberryPi)
     circuit_name = models.CharField(max_length=64)
     state = models.BinaryField();
 
 class Reading(models.Model):
-    reading_id = models.AutoField(primary_key=True);
     circuit_num = models.ForeignKey(Circuit)
     voltage = models.FloatField()
     current = models.FloatField()
-    datetime = models.DateTimeField()
+    datetime = models.DateTimeField(default=datetime.datetime.now)
 
 
 
