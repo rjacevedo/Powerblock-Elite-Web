8e16833fd10b27e4e20ca7839f631e73a9653b3a
Jonathan Acevedo <jonathan@ginger.io>
2014-07-19 19:58:08 -0400

    added useful middleware

--- PbElite/settings_common.py
+++ PbElite/settings_common.py
@@ -49,6 +49,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'custom.middleware.ProcessExceptionMiddleware',
 )
 
 ROOT_URLCONF = 'PbElite.urls'
