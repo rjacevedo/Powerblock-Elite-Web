61a788514f7345820fcf9b186c1f0234b2c4964c
Jonathan Acevedo <jonathan@ginger.io>
2014-07-18 21:07:44 -0400

    format json response for letao

--- PbElite/views.py
+++ PbElite/views.py
@@ -5,12 +5,16 @@ import json
 
 @csrf_exempt
 def test_response(request):
-    if request.method == 'GET':
-        now = datetime.datetime.now()
-        html = "<html><body>It is now %s.</body></html>" % now
-        return HttpResponse(html)
-    else:
-        response_data = {}
-        response_data['result'] = "It worked omg!"
-
-        return HttpResponse(json.dumps(response_data), content_type="application/json")
+    response_data = [
+        {
+            'user_id': "tonyleterrible",
+            'circuit_id': 123,
+            'switch': {'id': 1, 'toggle': 'False'}
+        },
+        {
+            'user_id': "futony",
+            'circuit_id': 145,
+            'switch': {'id': 3, 'toggle': 'True'}
+        }
+    ]
+    return HttpResponse(json.dumps(response_data), content_type="application/json")
