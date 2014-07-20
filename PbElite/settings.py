bfaa7d8c1297bfb0bfa0d0c67e4e533018d9f369
yd2zhang <yd.zhang92@gmail.com>
2014-07-19 21:50:26 -0400

    Delete settings.py

--- PbElite/settings.py
+++
@@ -1,9 +0,0 @@
-from settings_common import *
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'mydb',
-        'USER': 'root',
-        'PASSWORD' : '',
-    }
-}
