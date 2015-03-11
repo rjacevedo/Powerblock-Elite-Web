418099a0676bfe627937a1ab0399b5abc6b77e7c
Javier Ching <jhnching@uwaterloo.ca>
2015-03-11 17:04:54 -0400

    completed registration. updated error handling. added first and last name fields

--- PbElite/staticfiles/js/registration.js
+++ PbElite/staticfiles/js/registration.js
@@ -1,4 +1,37 @@
-﻿$("#registrationKey").click(clearField("registration"));
+﻿$(document).ready(function() {
+    $("#registrationKey").focus(function() {
+        clearField("registrationKey");
+        console.log("registratoin key selected");
+    });
+
+    $("#username").focus(function() {
+        clearField("username");
+    });
+
+    $("#password").focus(function() {
+        clearField("password");
+    });
+
+    $("#email").focus(function() {
+        clearField("email");
+    });
+
+    $("#address").focus(function() {
+        clearField("address");
+    });
+
+    $("#city").focus(function() {
+        clearField('city');
+    });
+
+    $('#country').focus(function() {
+        clearField('country');
+    });
+
+    $('#postalCode').focus(function() {
+        clearField('postalCode');
+    });
+});
 
 function completeRegistration() {
     var good = true;
@@ -10,51 +43,48 @@ function completeRegistration() {
     if ($("#username").val() == "") {
         good = false;
         incompleteField("username", "Please enter a username");
-    } else {
-        clearField("username");
-    }
+    } 
 
     if ($("#password").val() == "") {
         good = false;
         incompleteField("password", "Please enter a password");
-    } else {
-        clearField("password");
-    }
+    } 
 
     if ($("#email").val() == "") {
         good = false;
         incompleteField("email", "Please enter an email");
-    } else {
-        clearField("email");
+    } 
+
+    if($('#firstName').val() == "") {
+        good = false;
+        incompleteField("firstName", "Please enter your first name");
+    }
+
+    if($('#lastname').val() == "") {
+        good = false;
+        incompleteField("lastName", "Please enter your last name");
     }
 
     if ($("#address").val() == "") {
         good = false;
         incompleteField("address", "Please enter an address");
-    } else {
-        clearField("address");
     }
 
     if($("#city").val() == "") {
         good = false;
         incompleteField("city", "Please enter a city");
-    } else {
-        clearField("city");
     }
 
     if ($("#country").val() == "") {
         good = false;
         incompleteField("country", "Please enter a country");
-    } else {
-        clearField("country");
-    }
+    } 
 
     if ($("#postalCode").val() == "") {
         good = false;
         incompleteField("postalCode", "Please enter a postal code");
-    } else {
-        clearField("postalCode");
     }
+
     if (good) {
         $.ajax({
             type: "POST",
@@ -63,8 +93,8 @@ function completeRegistration() {
                 registrationKey: $("#registrationKey").val(),
                 username: $("#username").val(),
                 password: $("#password").val(),
-                firstname: 'placeholder',
-                lastname: 'placeholder',
+                firstname: $('#firstName').val(),
+                lastname: $('#lastName').val(),
                 address: $("#address").val(),
                 email: $("#email").val(),
                 city: $("#city").val(),
@@ -87,5 +117,5 @@ function incompleteField(element, placeholder) {
 
 function clearField(element) {
     $("#" + element).attr("placeholder", "");
-    $("#" + element).attr("border-color", "initial");
+    $("#" + element).css("border-color", "rgb(204, 204, 204)");
 }
